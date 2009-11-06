a6bc9a3f3e4dc31024957a59566053eb1452b683
Han Lin Shang <HanLin.Shang@buseco.monash.edu.au>
2009-11-06 00:00:00 +0000

    version 1.2

--- R/extract.x.R
+++ R/extract.x.R
@@ -1,8 +1,20 @@
-extract.x = function(data, xorder)
+extract.x = function (data, xorder) 
 {
-  x = data$x
-  y = data$y
-  newx = x[xorder]
-  fts(newx, y[xorder,], xname = data$xname, yname = data$yname)
+    x = data$x
+    y = data$y
+    index=vector(,length(xorder))
+    if (length(xorder) == 1){
+        index = which(as.numeric(rownames(y)) == xorder)
+    }
+    if (length(xorder) > 1){
+        for(i in 1:length(xorder)){
+            index[i] = which(as.numeric(rownames(y)) == xorder[i])
+        }    
+    }
+    newdata = as.matrix(y[index,])
+    newx = x[index]
+    ftsdata = fts(newx, newdata, xname = data$xname, yname = data$yname)
+    rownames(ftsdata$y) = as.character(xorder)
+    return(ftsdata)
 }
 
