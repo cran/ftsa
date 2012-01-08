ba3ee43d517cbbbfbc74eb1cf22726442d139a42
Han Lin Shang <HanLin.Shang@buseco.monash.edu.au>
2012-01-08 00:00:00 +0000

    version 2.9

--- R/forecastfplsr.R
+++ R/forecastfplsr.R
@@ -1,17 +1,20 @@
 forecastfplsr <-
 function(object, components = 2, h = 20)
 {
-xname = object$xname
-yname = object$yname
-foreca = matrix(,length(object$x),h)
-for(i in 1:h)
-{
-fore = fplsr(object, order = components)$Ypred
-foreca[,i] = fore$y
-newdata = cbind(object$y, fore$y)
-colnames(newdata) = as.numeric(colnames(object$y))[1]:(max(as.numeric(colnames(object$y))) + 1)
-object = fts(object$x, newdata)
+	xname = object$xname
+	yname = object$yname
+	foreca = matrix(, length(object$x), h)
+	data = object
+	for(i in 1:h)
+	{
+		fore = fplsr(data, order = components)$Ypred
+		foreca[,i] = fore$y
+		newdata = cbind(data$y, fore$y)
+		colnames(newdata) = as.numeric(colnames(data$y))[1]:(max(as.numeric(colnames(data$y))) + 1)
+		data = fts(data$x, newdata)
+	}
+	colnames(foreca) = (max(as.numeric(colnames(object$y)))+1):(max(as.numeric(colnames(object$y)))+h)
+	forecafts = fts(object$x, foreca, xname = xname, yname = yname)
+	return(forecafts)
 }
-forecafts = fts(object$x, foreca, xname = xname, yname = yname)
-return(forecafts)
-}
+
