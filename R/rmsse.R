253b8a4bbb10d8035ca7911a99f7c610cca158d3
Han Lin Shang <HanLin.Shang@monash.edu>
2012-02-01 00:00:00 +0000

    version 3.1

--- R/rmsse.R
+++ R/rmsse.R
@@ -1,13 +1,15 @@
-rmsse = function(forecast, true)
-{ 
-  if (length(forecast) != length(true))
-      stop("RMSSE: the lengths of input vectors must be the same.")
-  n = length(true)
-  truerror = vector(, n - 1)
-  for(i in 1:(n - 1)){
-      truerror[i] = (true[i + 1] - true[i])^2
-  }
-  qt = (true - forecast) / (sum(truerror) / (n - 1))
-  scalederror = (mean(qt))^(1/2)
-  return(round(scalederror, 6))
+rmsse = function(forecast, outsampletrue, insampletrue)
+{
+	if(length(forecast) != length(outsampletrue))
+		stop("RMSSE: the lengths of input vectors must be the same.")
+	n = length(insampletrue)
+	insamplerr = vector(, (n - 1))
+	for(i in 1:(n - 1))
+	{
+		insamplerr[i] = abs(insampletrue[i+1] - insampletrue[i])
+	}
+	qt = (outsampletrue - forecast)/(sum(insamplerr)/(n - 1))
+	scalederror = sqrt(mean(qt^2))
+	return(round(scalederror, 6))
 }
+
