9e03f23f9547770ed366fc748f731a440ca2a09d
Han Lin Shang <HanLin.Shang@monash.edu>
2012-02-01 08:13:26 +0000

    version 3.1

--- R/mase.R
+++ R/mase.R
@@ -1,13 +1,14 @@
-mase = function(forecast, true)
-{ 
-  if (length(forecast) != length(true))
-      stop("MASE: the lengths of input vectors must be the same.")
-  n = length(true)
-  truerror = vector(,n - 1)
-  for(i in 1:(n - 1)){
-      truerror[i] = abs(true[i + 1] - true[i])
-  }
-  qt = (true - forecast) / (sum(truerror) / (n - 1))
-  scalederror = mean(abs(qt))
-  return(round(scalederror, 6))
+mase = function(forecast, outsampletrue, insampletrue)
+{
+	if(length(forecast) != length(outsampletrue))
+		stop("MASE: the lengths of input vectors must be the same.")
+	n = length(insampletrue)
+	insamplerr = vector(, (n - 1))
+	for(i in 1:(n - 1))
+	{
+		insamplerr[i] = abs(insampletrue[i+1] - insampletrue[i])
+	}
+	qt = (outsampletrue - forecast)/(sum(insamplerr)/(n - 1))
+	scalederror = mean(abs(qt))
+	return(round(scalederror, 6))
 }
