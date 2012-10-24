9120e042a4e1ace638f4af6dae1ed85da1e97d81
Han Lin Shang <HanLin.Shang@monash.edu>
2012-10-24 06:39:13 +0000

    version 3.6

--- R/sd.fts.R
+++ R/sd.fts.R
@@ -1,4 +1,4 @@
-`sd.fts` <- function(x, method = c("coordinate", "FM", "mode", "RP", "RPD"), trim = 0.25,...)
+`sd.fts` <- function(x, method = c("coordinate", "FM", "mode", "RP", "RPD", "radius"), trim = 0.25, alpha, weight, ...)
 {
    if (class(x)[1] == "fts"|class(x)[1] == "fds"|class(x)[1] == "sfts"){
        functions = t(x$y)
@@ -23,6 +23,10 @@
        if (method == "RPD"){
            lista = depth.RPD(x, trim = trim)$ltrim
            loc = sqrt(func.var(functions[lista,]))
+       }
+       if (method == "radius"){
+       	   lista = which(depth.radius(x, alpha, trim, weight)$weight==1)
+       	   loc = sqrt(func.var(functions[lista,]))
        }
        if (class(x)[1] == "fds"){
            warning("Object is not a functional time series.")
